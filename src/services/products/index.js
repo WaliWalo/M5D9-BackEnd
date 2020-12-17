1d6ad7f7da0fa0657f5d4e6d670b463f3567e05c
Hung Jin Chong <hungjinchong@Outlook.com>
2020-12-17 16:00:12 +0000

    Done backend function for downloadin pdf and csv

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -8,12 +8,24 @@ const multer = require("multer");
 const { readDB, writeDB } = require("../../lib/utilities");
 const { writeFile } = require("fs-extra");
 const upload = multer({});
+const { parseString } = require("xml2js");
+const axios = require("axios");
+const { promisify } = require("util");
+const { begin } = require("xmlbuilder");
+const { parse } = require("path");
+const asyncParser = promisify(parseString);
+const { Transform } = require("json2csv");
+const { pipeline } = require("stream");
+const PDFDocument = require("pdfkit");
+const { createReadStream, createWriteStream } = require("fs-extra");
+const nodemailer = require("nodemailer");
 
 const productsPublicFile = path.join(__dirname, "../../../public/img/products");
 
 const productsFilePath = path.join(__dirname, "products.json");
 const reviewsFilePath = path.join(__dirname, "reviews.json");
 const cartsFilePath = path.join(__dirname, "carts.json");
+const fontFilePath = path.join(__dirname, "../../fonts/Roboto-Regular.ttf");
 /**
  * 
  *  {
@@ -171,6 +183,169 @@ router.delete("/:id", async (req, res, next) => {
     } else {
       await writeDB(productsFilePath, newProducts);
       res.status(200).send(`Product with id:${req.params.id} deleted`);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//     GET /products/sumTwoPrices => pass two product IDs in req.query,
+//     the route should use them to get the prices of the two products and calculate the sum by contacting the following end-point:
+//     http://www.dneonline.com/calculator.asmx?op=Add (ONLY INTEGER ALLOWED, SO CONVERT TO INTEGER NUMBERS IF NEEDED).
+//     Both request and response are gonna be in XML format, so you should create XML with data
+//     for the request and when you get the response you have to parse the response to JSON and send it back to the caller.
+//     (I'm totally aware that doing a sum in this way is sooo stupid, but it's an XML
+//     exercise of course ;) )
+router.get("/xml/sumTwoPrices", async (req, res, next) => {
+  try {
+    // POST /calculator.asmx HTTP/1.1
+    // Host: www.dneonline.com
+    // Content-Type: text/xml; charset=utf-8
+    // Content-Length: length
+    // SOAPAction: "http://tempuri.org/Add"
+
+    // <?xml version="1.0" encoding="utf-8"?>
+    // <soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+    //   <soap:Body>
+    //     <Add xmlns="http://tempuri.org/">
+    //       <intA>int</intA>
+    //       <intB>int</intB>
+    //     </Add>
+    //   </soap:Body>
+    // </soap:Envelope>
+    const { product1, product2 } = req.query;
+    const xmlBody = begin()
+      .ele("soap:Envelope", {
+        "xmlns:soap": "http://schemas.xmlsoap.org/soap/envelope/",
+      })
+      .ele("soap:Body")
+      .ele("Add", { xmlns: "http://tempuri.org/" })
+      .ele("intA")
+      .text(product1)
+      .up()
+      .ele("intB")
+      .text(product2)
+      .end();
+
+    const response = await axios({
+      method: "post",
+      url: "http://www.dneonline.com/calculator.asmx?op=Add",
+      data: xmlBody,
+      headers: { "Content-type": "text/xml" },
+    });
+
+    //parse xml to json
+    const xml = response.data;
+    const parsedJS = await asyncParser(xml);
+    const formatParsedJs =
+      parsedJS["soap:Envelope"]["soap:Body"][0]["AddResponse"][0][
+        "AddResult"
+      ][0];
+    res.statusMessage(200).send(formatParsedJs);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// GET /products/exportToCSV => exports list of products in form of a CSV file
+router.get("/csv/exportToCSV", async (req, res, next) => {
+  try {
+    const jsonReadableStream = createReadStream(productsFilePath); //source
+    //source -> transform to csv -> to user
+    const json2csv = new Transform({
+      fields: [
+        "_id",
+        "name",
+        "description",
+        "brand",
+        "price",
+        "category",
+        "createdAt",
+        "imageUrl",
+      ],
+    });
+    //to prompt where to save file
+    res.setHeader("Content-Disposition", "attachment; filename=products.csv");
+    pipeline(jsonReadableStream, json2csv, res, (err) => {
+      if (err) {
+        console.log(err);
+        next(err);
+      } else {
+        console.log("Done");
+        res.status(200).send("Done");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// GET /products/:id/exportToPDF => converts single product data into a PDF and sends to client as a downloadable file.
+// You can choose ANY pdf npm module you think should fit. style properly the PDF and add product's image.
+router.get("/:id/exportPdf", async (req, res, next) => {
+  try {
+    const allProducts = await readDB(productsFilePath);
+    let selectedProduct = allProducts.find(
+      (product) => product._id === req.params.id
+    );
+    if (selectedProduct) {
+      const doc = new PDFDocument();
+      doc.pipe(fs.createWriteStream(`${selectedProduct._id}.pdf`));
+      doc.font(fontFilePath).fontSize(25).text(selectedProduct.name, 100, 100);
+      doc.image(selectedProduct.imageUrl, {
+        fit: [250, 300],
+        align: "center",
+        valign: "center",
+      });
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${selectedProduct._id}.pdf`
+      );
+      doc.pipe(res);
+      doc.end();
+
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      let testAccount = await nodemailer.createTestAccount();
+
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: testAccount.user, // generated ethereal user
+          pass: testAccount.pass, // generated ethereal password
+        },
+      });
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: "hungjinchong@outlook.com, baz@example.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: `<b>Hi, this is your requested product. ${selectedProduct.name} ${selectedProduct.imageUrl}<img src = "${selectedProduct.imageUrl}"/></b>
+        <p>Here's a nyan cat for you as an embedded attachment:<br/><img src="https://i.pinimg.com/originals/e8/65/bd/e865bd7c7395936f91b116ba6d827aad.gif"/></p>`, // html body
+        attachments: [
+          // File Stream attachment
+          {
+            filename: `${selectedProduct._id}.pdf`,
+            path: path.join(__dirname, `../../../${selectedProduct._id}.pdf`),
+            cid: selectedProduct._id, // should be as unique as possible
+            contentType: "application/pdf",
+          },
+        ],
+      });
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      res.status(200).send("Success");
+    } else {
+      let error = new Error();
+      error.httpStatusCode = 404;
+      next(error);
     }
   } catch (error) {
     console.log(error);
